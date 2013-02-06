c039c3cdbc8634dc852f58d8aba811053dec6584
Christopher Wood <christopherwood07@gmail.com>
2013-02-06 17:49:28 -0500

    Outlined folkman code.

---
+++ src/folkman/solver/folkman.cpp
@@ -0,0 +1,9 @@
+// Folkman.cpp
+
+#define MAXN 1000
+#include "nauty.h"
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
--- src/folkman/solver/folkman.py
+++ src/folkman/solver/folkman.py
@@ -2,15 +2,23 @@
 
 from networkx import nx
 
-def walkEdges(edgeColor, edgeIndex, maxColor):
+def checkPresence(F, G, edgeColor, color):
+	print("checking the presence of graph for color " + str(color) + " in another graph")
+	# TODO: the check should go here..
+	# TODO: leverage nauty here...
+
+def walkEdges(F, G, H, edgeColor, edgeIndex, maxColor):
 	if (edgeIndex == (len(edgeColor) - 1)):
 		for i in range(0, maxColor):
 			edgeColor[edgeIndex] = i
 			print(edgeColor)
+			for color in range(0, maxColor):
+				checkPresence(F, G, edgeColor, color)
+				checkPresence(F, H, edgeColor, color)
 	else:
 		for i in range(0, maxColor):
 			edgeColor[edgeIndex] = i
-			walkEdges(edgeColor, edgeIndex + 1, maxColor)
+			walkEdges(F, G, H, edgeColor, edgeIndex + 1, maxColor)
 
 def folkman(F, G, H):
 	''' Decide whether or not F -> (G,H)
@@ -22,7 +30,7 @@ def folkman(F, G, H):
 		edgeList.append(edge)
 		edgeColor.append(0) # start with color 0
 
-	walkEdges(edgeColor, 0, 2)
+	walkEdges(F, G, H, edgeColor, 0, 2)
 
 	# Initialize the edge and color index
 	#edgeIndex = len(F.edges() - 1)
