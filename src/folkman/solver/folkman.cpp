57260e50e447e0e7ac1f2f020ce3044d8ed69385
Christopher Wood <christopherwood07@gmail.com>
2013-02-08 20:57:40 -0500

    Finished conversion to C++ - need to clean up the code and then test.

--- src/folkman/solver/folkman.cpp
+++ src/folkman/solver/folkman.cpp
@@ -11,6 +11,11 @@ typedef struct
 	int y;
 } edge;
 
+// For convenience...
+using namespace std;
+
+vector<bool> results;
+
 void display(int order, int** F)
 {
 	printf("---\n");
@@ -24,14 +29,113 @@ void display(int order, int** F)
 	}
 }
 
+bool isInduced(int order, int n, int edgeCount, int** F, int** colorState, edge* edgeMap, int color)
+{
+	bool induced = false;
+
+	if (n == 3) // special case for triangles
+	{
+		//printf("checking for triangle\n");
+		for (int edgeIndex = 0; edgeIndex < edgeCount; edgeIndex++) // search over each  edge
+		{
+			int x = edgeMap[edgeIndex].x;
+			int y = edgeMap[edgeIndex].y;
+			//printf("examining %d,%d edge\n", x, y);
+			if (colorState[x][y] == color)
+			{
+				for (int v = 0; v < order && (x != v) && (y != v); v++) // check each of the remaining vertices
+				{
+					//printf("checking possible triangle %d %d (%d)\n", x, y, v);
+					//printf("1 %d\n", F[x][v]);
+					//printf("2 %d\n", F[y][v]);
+					if (F[x][v] == 1 && F[y][v] == 1) // both edges exist
+					{
+						//display(order, colorState);
+						//printf("1 %d\n", colorState[x][v]);
+						//printf("2 %d\n", colorState[y][v]);
+						if (colorState[x][v] == color && colorState[y][v] == color)
+						{
+							return true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return induced;
+
+/*
+if (n == 3): # Search for triangles with a stupid simple for loop...
+		edgeIndex = 0
+		for edgeIndex in range(0, len(edgeColor)):
+			edge = F.edges()[edgeIndex]
+			if (edgeColor[edgeIndex] == color): # Only look at the specific color
+				for vertex in F.nodes():
+					# Format the edge for linst the index check
+					edge1 = ()
+					if (edge[0] < vertex):
+						edge1 = (edge[0], vertex)
+					else:
+						edge1 = (vertex, edge[0])
+					edge2 = ()
+					if (edge[1] < vertex):
+						edge2 = (edge[1], vertex)
+					else:
+						edge2 = (vertex, edge[1])
+
+					# Check to see if we have a triangle of the same color
+					if (edge1 in F.edges() and edge2 in F.edges()):
+						if (edgeColor[edgeColorMap[edge1]] == color and edgeColor[edgeColorMap[edge2]] == color):
+							return True
+*/
+}
+
 // Walk over all possible colors in the graph F, invoking the k-clique check algorithm for each color
 bool walkColors(int order, int color, int** F, int* colors, int** colorState, int edgeCount, edge* edgeMap, int edgeIndex)
 {
 	// Base case
-	if (edgeIndex == edgeCount)
+	if (edgeIndex == edgeCount - 1)
 	{
-		bool induced = false;
-		display(order, colorState);
+		//printf("new coloring...\n");
+		//display(order, colorState);
+		int x = edgeMap[edgeIndex].x;
+		int y = edgeMap[edgeIndex].y;
+		//printf("okay... %d %d\n", x, y);
+		for (int c = 0; c < color; c++)
+		{
+			colorState[x][y] = c;
+			colorState[y][x] = c;
+
+			//printf("here we go with induction check\n");
+			bool induced = false;
+			for (int ci = 0; ci < color; ci++)
+			{
+				if (isInduced(order, colors[ci], edgeCount, F, colorState, edgeMap, ci))
+				{
+					//printf("found a triangle in one color!\n");
+					induced = true;
+				}
+			}
+			results.push_back(induced);
+		}
+
+/*
+		for i in range(0, maxColor):
+			edgeColor[edgeIndex] = i
+			induced = False
+
+			# We've completed the color assignment... now check to see if there's a K_m in color1/2 or K_n in color1/2
+			for color in range(0, maxColor):
+				if (checkPresence(F, m, edgeColor, edgeColorMap, color)):
+					induced = True
+				elif checkPresence(F, n, edgeColor, edgeColorMap, color):
+					induced = True
+
+			# Dump in the result
+			colorSet.append(induced)
+*/
+
 		// walk the color set and check...
 		// display the adjacency matrix for debug
 
@@ -41,7 +145,7 @@ bool walkColors(int order, int color, int** F, int* colors, int** colorState, in
 		bool induced = false;
 		int x = edgeMap[edgeIndex].x;
 		int y = edgeMap[edgeIndex].y;
-		printf("looking at edge %d,%d\n", x, y);
+		//printf("looking at edge %d,%d\n", x, y);
 		for (int c = 0; c < color; c++)
 		{
 			colorState[x][y] = c;
@@ -87,7 +191,7 @@ bool walkColors(int order, int color, int** F, int* colors, int** colorState, in
 bool folkman(int order, int color, int** F, int* colors)
 {
 	bool result = true;
-	display(order, F);
+	//display(order, F);
 
 	// Allocate space for the color state -  we only use the upper diagnoal of the matrix
 	int** colorState = new int*[order];
@@ -116,7 +220,7 @@ bool folkman(int order, int color, int** F, int* colors)
 		}
 	}
 	printf("edge count = %d\n", edgeCount);
-	display(order, colorState);
+	//display(order, colorState);
 
 	// CAW: THIS IS CORRECT UP TO HERE
 
@@ -133,7 +237,7 @@ bool folkman(int order, int color, int** F, int* colors)
 				edge e;
 				e.x = i;
 				e.y = j;
-				printf("adding edge (%d,%d) to edgeMap\n", e.x, e.y);
+				//printf("adding edge (%d,%d) to edgeMap\n", e.x, e.y);
 				edgeMap[edgeIndex++] = e;
 			}
 		}
@@ -147,7 +251,7 @@ bool folkman(int order, int color, int** F, int* colors)
 
 	// Now, walk the colors... but to do it sequentially or recursively... that is the question.
 	// Python implementation does it recursively
-	printf("starting the color walking routine\n");
+	//printf("starting the color walking routine\n");
 	walkColors(order, color, F, colors, colorState, edgeCount, edgeMap, 0);
 
 	// CAW: color permutation appears to be correct
@@ -159,15 +263,17 @@ bool folkman(int order, int color, int** F, int* colors)
 	return result;
 }
 
+#define FSIZE 8
+
 int main(int argc, char** argv)
 {
 	// K_3 for testing color permutation algorithm
-	int** F = new int*[3];
-	for (int i = 0; i < 3; i++)
-		F[i] = new int[3];
-	for (int i = 0; i < 3; i++)
+	int** F = new int*[FSIZE];
+	for (int i = 0; i < FSIZE; i++)
+		F[i] = new int[FSIZE];
+	for (int i = 0; i < FSIZE; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < FSIZE; j++)
 		{
 			if (i == j) F[i][j] = 0;
 			else F[i][j] = 1;
@@ -175,11 +281,33 @@ int main(int argc, char** argv)
 	}
 
 	int* colors = new int[2];
-	colors[0] = 0;
-	colors[1] = 1;
+	colors[0] = 3;
+	colors[1] = 3;
 
 	//int F[3][3] = {{0, 1, 1}, {1, 0, 1}, {1, 1, 0}};
 	//int colors[2] = {0, 1};
-	folkman(3, 2, F, colors);
+	//display(FSIZE, F);
+	printf("K_%d -> (3, 3)?\n", FSIZE);
+	folkman(FSIZE, 2, F, colors);
+
+	bool foundFalse = false;
+	for (int i = 0; i < results.size(); i++)
+	{
+		if (results[i] == false) 
+		{
+			foundFalse = true;
+			break;
+		}
+		//printf("result = %d\n", results[i]);
+	}
+	if (foundFalse)
+	{
+		printf("No.\n");
+	}
+	else
+	{
+		printf("Yes.\n");
+	}
+
 	return 0;
 }
