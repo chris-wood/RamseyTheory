c6338afdc8cf3f9e73828de41713bea6b98c88e6
Christopher Wood <christopherwood07@gmail.com>
2013-06-07 17:03:13 -0400

    Started writing random graph generation code to break Fe(3,3;4)

--- src/folkman/fe334/gg.py
+++ src/folkman/fe334/gg.py
@@ -2,135 +2,124 @@
 # Author: Christopher Wood, caw4567@rit.edu
 
 from networkx import nx
+from reducer import reducer
+from reducer import makeDimacsCNF
 import random
+import pickle
 import sys
 
-def buildRandomGraph(npendants, saturate = 1):
+def makeEdge(u, v):
+	if (u <= v):
+		return (u,v)
+	else:
+		return (v,u)
+
+def inEdges(g, e):
+	if (e[0] < e[1]):
+		if e in g.edges():
+			return True
+	else:
+		e2 = (e[1], e[0])
+		if e2 in g.edges():
+			return True
+	return False
+
+def containsEdges(g, edges):
+	for e in edges:
+		if not (inEdges(g, e)):
+			return False
+	return True # All of them were in the edge set
+
+def hasK4(g):
+	# Simply check to see if there exists an edge between all of them...
+	for x1 in g.nodes():
+		for x2 in g.nodes():
+			for x3 in g.nodes():
+				for x4 in g.nodes():
+					if x1 != x2 and x1 != x3 and x1 != x4 and x2 != x3 and x2 != x4 and x3 != x4:
+						edges = []
+					 	edges.append(makeEdge(x1,x2))
+						edges.append(makeEdge(x1,x3))
+						edges.append(makeEdge(x1,x4))
+						edges.append(makeEdge(x2,x3))
+						edges.append(makeEdge(x2,x4))
+						edges.append(makeEdge(x3,x4))
+						if containsEdges(g, edges):
+							return True
+	return False
+
+def k4WithEdge(g, e):
+	# Simply check to see if there exists an edge between all of them...
+	u = e[0]
+	v = e[1]
+	for x1 in g.nodes():
+		for x2 in g.nodes():
+			edges = []
+		 	edges.append(makeEdge(u,v))
+			edges.append(makeEdge(u,x1))
+			edges.append(makeEdge(u,x2))
+			edges.append(makeEdge(v,x1))
+			edges.append(makeEdge(v,x2))
+			edges.append(makeEdge(x1,x2))
+			if containsEdges(g, edges):
+				return True
+	return False
+
+def saturateAvoidK4(g, vertices):
+	for x in vertices:
+		for y in vertices:
+			if (x != y):
+				print >> sys.stderr, "Trying to add edge: " + str(x) + "-" + str(y)
+				e = makeEdge(x, y)
+				if not (k4WithEdge(g, e)):
+					g.edges().append(e)
+
+def buildRandomGraph(nv, npendants, saturate = 1):
 	g = nx.Graph()
 	for v in range(npendants):
 		g.add_node(v)
 
+	nToAdd = nv - npendants
+	vertices = []
+	for u in range(npendants, nToAdd + npendants):
+		g.add_node(u)
+		vertices.append(u)
+		for v in range(npendants): # pendants get connected to every new vertex that's being added
+			g.add_edge(v, u)
+
 	if (saturate == 1):
-		
+		saturateAvoidK4(g, vertices)
 
-	def __init__(self, n, r):
-		self.n = n
-		self.r = r
-		self.graph = nx.Graph()
+	if (hasK4(g)):
+		raise Exception("No K4 should exist... Check the code.")
+	print >> sys.stderr, g.edges()
 
-		# Add the vertices
-		for v in range(0, n):
-			self.graph.add_node(v)
+	return g
 
-		# Add the edges
-		for x in range(0, n):
-			for y in range(0, n):
-				if (x != y):
-					for alpha in range(0, n):
-						# E(G) = {(x,y) | x-y = alpha^r mod n}
-						if (((x - y) % n) == ((alpha ** r) % n)):
-							self.graph.add_edge(x, y)
+def writeCNF(g, fname, numVars, cnf):
+	filename = str(fname) + ".pickle"
+	pickle.dump(g, open(filename, 'w')) # save the pickled version
+	filename = str(fname) + ".out.txt"
+	print >> sys.stderr, filename
+	f = open(filename, 'wb')
+	header, clauses = makeDimacsCNF(numVars, cnf)
+	for c in clauses:
+		for l in c:
+			f.write(str(l) + " ")
+		f.write("0 \n")
 
-	def removeIndependentSet(self):
-		iset = nx.maximal_independent_set(self.graph)
-		for v in iset:
-			self.graph.remove_node(v)
+# Main entry point
+if __name__ == "__main__":
+	nv = int(sys.argv[1])
+	npendants = int(sys.argv[2])
+	saturate = int(sys.argv[3])
+	iterations = int(sys.argv[4])
+	r = reducer()
 
-	def dropNode(self, n):
-		if (n < len(self.graph.nodes())):
-			self.graph.remove_node(self.graph.nodes()[n])
-
-	def dropEdge(self, e):
-		if (e < len(self.graph.edges())):
-			u = self.graph.edges()[e][0]
-			v = self.graph.edges()[e][1]
-			self.graph.remove_edge(u, v)
-
-	# Greedily add as many edges as possible while avoiding K4!
-	def saturateAvoidK4(self):
-		for x in self.graph.nodes():
-			for y in self.graph.nodes():
-				if (x != y):
-					print >> sys.stderr, "Trying to add edge: " + str(x) + "-" + str(y)
-					e = self.makeEdge(x, y)
-					if not (self.k4WithEdge(e)):
-						self.graph.edges().append(e)
-
-	def addRandomEdgeAvoidK4(self):
-		e = self.pickRandomEdge()
-		count = 0
-		while not (e in self.graph.edges()):
-			e = self.pickRandomEdge()
-			count = count + 1
-			if (count > 100):
-				raise Exception("Couldn't find a new edge that didn't already exist after 100 tries...")
-		if not (self.k4WithEdge(e)):
-			self.graph.edges().append(e)
-			return True
-		else:
-			return False
-
-	def k4WithEdge(self, e):
-		# Simply check to see if there exists an edge between all of them...
-		u = e[0]
-		v = e[1]
-		for x1 in self.graph.nodes():
-			for x2 in self.graph.nodes():
-				edges = []
-			 	edges.append(self.makeEdge(u,v))
-				edges.append(self.makeEdge(u,x1))
-				edges.append(self.makeEdge(u,x2))
-				edges.append(self.makeEdge(v,x1))
-				edges.append(self.makeEdge(v,x2))
-				edges.append(self.makeEdge(x1,x2))
-				if self.containsEdges(edges):
-					return True
-		return False
-
-	def makeEdge(self, u, v):
-		if (u <= v):
-			return (u,v)
-		else:
-			return (v,u)
-
-	def containsEdges(self, edges):
-		for e in edges:
-			if not (self.inEdges(e)):
-				return False
-		return True # All of them were in the edge set
-
-	def inEdges(self, e):
-		if (e[0] < e[1]):
-			if e in self.graph.edges():
-				return True
-		else:
-			e2 = (e[1], e[0])
-			if e2 in self.graph.edges():
-				return True
-		return False
-
-	def pickRandomEdge(self):
-		u = random.randint(0, self.n)
-		v = random.randint(0, self.n)
-		count = 0
-		while (u == v):
-			v = random.randint(0, self.n)
-			count = count + 1
-			if (count > 100):
-				raise Exception("We tried to create a random edge 100 times, but alas, it did not work...")
-		if (v < u): # swap, u is always less than v in networkx graphs
-			tmp = u
-			u = v
-			v = tmp
-		return (u, v)
-
-	def addEdgeAvoidK4(self, u, v):
-		raise TypeError()
-
-	def removeNodes(self, n):
-		if (n < len(self.graph.nodes())):
-			for i in range(n):
-				self.graph.remove_node(self.graph.nodes()[0])
-
-	def getGraph(self):
-		return self.graph
+	# No error checking... use wisely
+	for i in range(iterations):
+		g = buildRandomGraph(nv, npendants, saturate)
+		print >> sys.stderr, "Reducing to 3-SAT"
+		numVars, cnf = r.reduce(g)
+		print(cnf)
+		writeCNF(g, "random_nv" + str(nv) + "_" + str(npendants) + "_" + str(saturate) + "_" + str(i), numVars, cnf)
