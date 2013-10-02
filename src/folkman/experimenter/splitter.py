12b1dce4b9ae0000e95e594bc5f5c10d09b70fe8
Christopher Wood <christopherwood07@gmail.com>
2013-10-02 10:54:09 -0700

    Pushing timed splitter with params

--- src/folkman/experimenter/splitter.py
+++ src/folkman/experimenter/splitter.py
@@ -25,7 +25,8 @@ import time
 #Make a SAT instance alpha for this precoloring.= GNR(127, 3)
 
 G = GNR(127,3)
-nv = 157 # THIS SHOULD BE 157 as per SPR's comments
+
+nv = int(sys.argv[1]) # THIS SHOULD BE 157 as per SPR's comments
 print >> sys.stderr, "Searching for split"
 start = time.time()
 v, bags, B, R, pcmap = G.find_candidate_rb_split(nv, target = 21, delta = 0.9) 
